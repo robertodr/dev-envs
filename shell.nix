0ea9b0e1c0ce7671f53e586ad10c31a06af70300
Roberto Di Remigio <roberto.diremigio@gmail.com>
2019-07-18 11:37:15 +0200

    git-along

--- shell.nix
+++ shell.nix
@@ -1,55 +1,51 @@
-let
-  hostPkgs = import <nixpkgs> {};
-  nixpkgs = (hostPkgs.fetchFromGitHub {
-    owner = "NixOS";
-    repo = "nixpkgs-channels";
-    # SHA for commit on 2019-04-09 on the nixos-19.03 branch
-    rev = "5c52b25283a6cccca443ffb7a358de6fe14b4a81";
-    sha256 = "0fhbl6bgabhi1sw1lrs64i0hibmmppy1bh256lq8hxy3a2p1haip";
-  });
-in
-  with import nixpkgs {
-    overlays = [(self: super:
-      {
-         python3 = super.python3.override {
-           packageOverrides = py-self: py-super: {
-             python-language-server = py-super.python-language-server.override {
-               providers = [
-                 "rope"
-                 "pyflakes"
-                 "mccabe"
-                 "pycodestyle"
-                 "pydocstyle"
-               ];
-             };
+with import (builtins.fetchGit {
+  name = "nixos-19.03-2019-06-26";
+  url = "https://github.com/NixOS/nixpkgs-channels";
+  ref = "nixos-19.03";
+  # Commit hash for nixos-19.03 as of 2019-07-18
+  # `git ls-remote https://github.com/nixos/nixpkgs-channels nixos-19.03`
+  rev = "77295b0bd26555c39a1ba9c1da72dbdb651fd280";
+}) {
+  overlays = [(self: super:
+    {
+       python3 = super.python3.override {
+         packageOverrides = py-self: py-super: {
+           python-language-server = py-super.python-language-server.override {
+             providers = [
+               "rope"
+               "pyflakes"
+               "mccabe"
+               "pycodestyle"
+               "pydocstyle"
+             ];
            };
          };
-      }
-    )];
-  };
+       };
+    }
+  )];
+};
 
-  stdenv.mkDerivation {
-    name = "parselglossy";
-    buildInputs = [
-      cloc
-      gitAndTools.pre-commit
-      pipenv
-      python3Packages.black
-      python3Packages.epc
-      python3Packages.guzzle_sphinx_theme
-      python3Packages.importmagic
-      python3Packages.isort
-      python3Packages.mypy
-      python3Packages.pyls-black
-      python3Packages.pyls-isort
-      python3Packages.pyls-mypy
-      python3Packages.python-language-server
-      python3Packages.sphinx
-      travis
-    ];
-
-    src = null;
-    shellHook = ''
-    SOURCE_DATE_EPOCH=$(date +%s)
-    '';
-  }
+stdenv.mkDerivation {
+  name = "parselglossy";
+  buildInputs = [
+    gitAndTools.pre-commit
+    pipenv
+    python3Packages.black
+    python3Packages.epc
+    python3Packages.guzzle_sphinx_theme
+    python3Packages.hypothesis
+    python3Packages.importmagic
+    python3Packages.isort
+    python3Packages.mypy
+    python3Packages.pyls-black
+    python3Packages.pyls-isort
+    python3Packages.pyls-mypy
+    python3Packages.pytest
+    python3Packages.pytest-flake8
+    python3Packages.pytest-mypy
+    python3Packages.pytestcov
+    python3Packages.python-language-server
+    python3Packages.sphinx
+    travis
+  ];
+}
