c18ad7245004dbe15d9b767363411cb44850ce1d
Roberto Di Remigio <roberto.diremigio@gmail.com>
2019-11-19 10:57:40 +0100

    git-along

--- shell.nix
+++ shell.nix
@@ -1,70 +1,103 @@
 with import (builtins.fetchGit {
-  name = "nixos-19.03-2019-07-07";
+  name = "nixpkgs-unstable-2019-11-11";
   url = "https://github.com/NixOS/nixpkgs-channels";
-  ref = "nixos-19.03";
-  # Commit hash for nixos-19.03 as of 2019-07-07
-  # `git ls-remote https://github.com/nixos/nixpkgs-channels nixos-19.03`
-  rev = "799a080ba1b1f969e737279cb6e7758a30e28754";
+  ref = "nixpkgs-unstable";
+  # Commit hash for nixpkgs-unstable as of 2019-11-11
+  # `git ls-remote https://github.com/nixos/nixpkgs-channels nixpkgs-unstable`
+  rev = "1e7ddf233a5c85ec2a49b8bdabf57ca0c028c9ac";
 }) {
   overlays = [(self: super:
     {
-       python3 = super.python3.override {
-         packageOverrides = py-self: py-super: {
-           python-language-server = py-super.python-language-server.override {
-             providers = [
-               "rope"
-               "pyflakes"
-               "mccabe"
-               "pycodestyle"
-               "pydocstyle"
-             ];
-           };
-         };
-       };
+      python3 = super.python3.override {
+        packageOverrides = py-self: py-super: {
+          python-language-server = py-super.python-language-server.override {
+            providers = [
+              "rope"
+              "pyflakes"
+              "mccabe"
+              "pycodestyle"
+              "pydocstyle"
+            ];
+          };
+        };
+      };
     }
   )];
 };
 
-stdenv.mkDerivation {
-  name = "mccrust";
-  buildInputs = [
-    rustup
+let src = fetchFromGitHub {
+  owner = "mozilla";
+  repo = "nixpkgs-mozilla";
+  # Commit hash for master as of 2019-10-10
+  # `git ls-remote https://github.com/mozilla/nixpkgs-mozilla master`
+  rev = "d46240e8755d91bc36c0c38621af72bf5c489e13";
+  sha256 = "0icws1cbdscic8s8lx292chvh3fkkbjp571j89lmmha7vl2n71jg";
+};
+in
+with import "${src.out}/rust-overlay.nix" pkgs pkgs;
+mkShell {
+  name = "MCCrust";
+  nativeBuildInputs = [
+    # Note: to use stable, just replace `nightly` with `stable`
+    #latest.rustChannels.nightly.rust
+   ((rustChannelOf
+     {
+       date = "2019-08-01";
+       channel = "nightly";
+     }).rust.override {
+       extensions = [
+         "rls-preview"
+         "rust-analysis"
+         "rustfmt-preview"
+       ];
+     })
 
+    # Build-time Additional Dependencies
+    arrayfire
+    #pkgconfig
+  ];
+
+  # Run-time Additional Dependencies
+  buildInputs = [
     pipenv
 
-    # Python dev-packages
+    python3
+
+    # System libraries needed for Python packages
+    freetype # Demanded by matplotlib
+
+    # Development tools
     gitAndTools.pre-commit
-    python3Packages.black
-    python3Packages.epc
-    python3Packages.guzzle_sphinx_theme
-    python3Packages.importmagic
-    python3Packages.isort
-    python3Packages.jedi
-    python3Packages.mypy
-    python3Packages.pyls-black
-    python3Packages.pyls-isort
-    python3Packages.pyls-mypy
-    python3Packages.pytest
-    python3Packages.python-language-server
-    python3Packages.sphinx
-
-    # Python packages
-    # NOTE some additional packages are installed by hand in .envrc
-    python3Packages.cython  # Needed to get randomgen
-    python3Packages.matplotlib
-    python3Packages.numpy
-    python3Packages.pandas
-
     lldb
-    openssl
-    pkgconfig
-    travis
+    python3.pkgs.black
+    python3.pkgs.epc
+    python3.pkgs.importmagic
+    python3.pkgs.isort
+    python3.pkgs.jedi
+    python3.pkgs.mypy
+    python3.pkgs.pyls-black
+    python3.pkgs.pyls-isort
+    python3.pkgs.pyls-mypy
+    python3.pkgs.python-language-server
   ];
 
   # Set Environment Variables
   RUST_BACKTRACE = 1;
-  src = null;
   shellHook = ''
-  SOURCE_DATE_EPOCH=$(date +%s)
+    SOURCE_DATE_EPOCH=$(date +%s) # required for python wheels
+
+    # FIXME This is temporarily needed to avoid problems with Cargo
+    #unset SSL_CERT_FILE
+
+    local venv=$(pipenv --bare --venv &>> /dev/null)
+
+    if [[ -z $venv || ! -d $venv ]]; then
+      pipenv install --dev &>> /dev/null
+    fi
+
+    export VIRTUAL_ENV=$(pipenv --venv)
+    export PIPENV_ACTIVE=1
+    export PYTHONPATH="$VIRTUAL_ENV/${python3.sitePackages}:$PYTHONPATH"
+    export PATH="$VIRTUAL_ENV/bin:$PATH"
   '';
 }
